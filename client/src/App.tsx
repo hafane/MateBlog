77dbc8bb87354132405d6743ca3efa3c5ac08a6e
Sarah <hafanepwnz@yahoo.com>
2025-09-09 04:04:21 +0300

    App improve

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from "react"
 import { Outlet } from "react-router"
 import { observer } from "mobx-react-lite"
-import useTheme from "./hooks/useTheme"
+import useTheme from "@hooks/useTheme"
+import Loader from "@components/loader/loader"
 
 const App = observer(() => {
 	useTheme()
 
 	return (
 		<div className="wrapper">
-			<Outlet />
+			<Suspense fallback={<Loader/>}>
+				<Outlet />
+			</Suspense>
 		</div>
 	)
 })
