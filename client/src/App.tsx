99829f926eca8db659bad58a9100fadc56bda08e
Sarah <hafanepwnz@yahoo.com>
2025-09-09 04:02:01 +0300

    Revert "App improve"
    This reverts commit 2a3713840dc30d0374595ce4205eff51fbe6f1a2.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,13 @@
-import { Suspense } from "react"
 import { Outlet } from "react-router"
 import { observer } from "mobx-react-lite"
-import useTheme from "@hooks/useTheme"
-import Loader from "@components/loader/loader"
+import useTheme from "./hooks/useTheme"
 
 const App = observer(() => {
 	useTheme()
 
 	return (
 		<div className="wrapper">
-			<Suspense fallback={<Loader/>}>
-				<Outlet />
-			</Suspense>
+			<Outlet />
 		</div>
 	)
 })
